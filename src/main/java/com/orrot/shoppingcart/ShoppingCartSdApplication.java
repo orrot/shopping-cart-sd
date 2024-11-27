8f7ccfbd3d4b64387fe6979b653134edfb10a9b5
Orlando Rodriguez <orlando.rodriguez.torres@gmail.com>
2024-11-27 17:37:56 -0500

    First commit
    Commit to initialize the project

---
+++ src/main/java/com/orrot/shoppingcart/ShoppingCartSdApplication.java
@@ -0,0 +1,13 @@
+package com.orrot.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartSdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orrot/shoppingcart/ShoppingCartSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orrot.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
